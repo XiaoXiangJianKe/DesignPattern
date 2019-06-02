62c39222853dacdf6b1ef9f7022515ab49996896
XiaoXiangJianKe <1416038523@qq.com>
2019-06-02 23:15:40 +0800

    适配器模式
    Signed-off-by: XiaoXiangJianKe <1416038523@qq.com>

---
+++ 07-AdapterPattern/Adaptee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdapterPattern
+{
+    /// <summary>
+    /// 适配者
+    /// </summary>
+    class Adaptee
+    {
+        public void SpecificRequest()
+        {
+            Console.WriteLine("特殊请求！");
+        }
+    }
+}
---
+++ 07-AdapterPattern/ObjectAdapter/Adapter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdapterPattern.ObjectAdapter
+{
+    /// <summary>
+    /// 适配器
+    /// </summary>
+    class Adapter : ITarget
+    {
+        private Adaptee adaptee = new Adaptee();
+
+        public void Request()
+        {
+            Console.WriteLine("对象适配器：");
+            adaptee.SpecificRequest();
+        }
+    }
+}
